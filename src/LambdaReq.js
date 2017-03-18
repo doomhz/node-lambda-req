907908c45a27a3df3f936981db39853514b71d55
Dumitru Glavan <dumitru.gl@gmail.com>
2017-03-18 04:14:38 +0100

    Add invoke

--- src/LambdaReq.js
+++ src/LambdaReq.js
@@ -5,11 +5,15 @@ class LambdaReq {
       typeof event === 'object' && event !== null,
       'Malformed Lambda event object.'
     )
-    console.assert(typeof callback === 'function', 'Malformed Lambda callback function.')
+    console.assert(
+      typeof callback === 'function',
+      'Malformed Lambda callback.'
+    )
     
     this._event = event
     this._callback = callback
     this._context = context
+    this._routes = {}
   }
 
   get isApiGateway () {
@@ -30,19 +34,70 @@ class LambdaReq {
     return this._event
   }
 
+  get headers () {
+    if (this.isApiGateway) {
+      return this._parseApiGatewayData().headers
+    }
+  }
+
   get currentRoute () {
     if (this.isApiGateway) {
       return `${this._parseApiGatewayData().method}_${this._parseApiGatewayData().path}`
     }
-  }
-
-  get currentTask () {
     if (this.isTask) {
-      return this._event.task
+      return `TASK_${this._event.task}`
     }
   }
 
-  respond (response, error) {
+  get (path, handler) {
+    this._route(`GET_${path}`, handler)
+  }
+
+  post (path, handler) {
+    this._route(`POST_${path}`, handler)
+  }
+
+  put (path, handler) {
+    this._route(`PUT_${path}`, handler)
+  }
+
+  delete (path, handler) {
+    this._route(`DELETE_${path}`, handler)
+  }
+
+  options (path, handler) {
+    this._route(`OPTIONS_${path}`, handler)
+  }
+
+  task (name, handler) {
+    this._route(`TASK_${name}`, handler)
+  }
+
+  invoke () {
+    // return error when there is no handler found
+    if (typeof this._routes[this.currentRoute] !== 'function') {
+      return this._respond(null, `Unhandled event: ${JSON.stringify(this._event)}`)
+    }
+    try {
+      // execute the route handler
+      const result = this._routes[this.currentRoute]({
+        params: Object.assign({}, this.params),
+        headers: Object.assign({}, this.headers),
+      }, Object.assign({}, this._event))
+      if (result && result.then) {
+        // handle a Promise result
+        return result.then((res)=> this._respond(res))
+      } else {
+        // handle any other result
+        return this._respond(result)
+      }
+    } catch (err) {
+      // catch any error and respond accordingly to the call type
+      return this._respond(null, err)
+    }
+  }
+
+  _respond (response, error) {
     if (this.isApiGateway) {
       return this._respondToApiGateway(response, error)
     }
@@ -50,6 +105,10 @@ class LambdaReq {
       return this._respondToTask(response, error)
     }
     return this._respondPlain(response, error)
+  }
+  
+  _route (id, handler) {
+    this._routes[id] = handler
   }
 
   _parseApiGatewayData (event = this._event) {
--- tests/unit/LambdaReq.js
+++ tests/unit/LambdaReq.js
@@ -17,7 +17,63 @@ describe('LambdaReq', () => {
     const callback = null
     const context = {}
     should(()=> new LambdaReq(event, callback, context))
-    .throw(/Malformed Lambda callback function/)
+    .throw(/Malformed Lambda callback/)
+  })
+
+  describe('invoke', () => {
+        
+    const event = {
+      httpMethod: 'GET',
+      resource: '/v1/test',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      queryStringParameters: {
+        name: 'john'
+      },
+      pathParameters: {
+        id: 'u-123'
+      },
+      body: '{"active":true}'
+    }
+    
+    it('responds', () => {
+      const callback = sinon.stub()
+      const handler = sinon.stub().returns({ success: true })
+      const lambda = new LambdaReq(event, callback)
+      lambda.get('/v1/test', handler)
+      lambda.invoke()
+      should(handler.calledWith(
+        { params: { name: 'john', id: 'u-123', active: true },
+          headers: { 'Content-Type': 'application/json' }
+        },
+        event
+      )).eql(true)
+      should(callback.getCall(0).args[1]).containEql({
+        body: '{"success":true}'
+      })
+    })
+
+    describe('when the handler result is a promise', () => {
+      it('waits for the promise to resolve then it responds', () => {
+        const callback = sinon.stub()
+        const handler = sinon.stub().returns(Promise.resolve({ success: true }))
+        const lambda = new LambdaReq(event, callback)
+        lambda.get('/v1/test', handler)
+        return lambda.invoke()
+        .then(()=> {
+          should(handler.calledWith(
+            { params: { name: 'john', id: 'u-123', active: true },
+              headers: { 'Content-Type': 'application/json' }
+            },
+            event
+          )).eql(true)
+          should(callback.getCall(0).args[1]).containEql({
+            body: '{"success":true}'
+          })
+        })
+      })
+    })
   })
 
   describe('isApiGateway', () => {
